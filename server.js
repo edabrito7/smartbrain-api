5331bca3e1872985c9a3930d087625f6a4ac4a13
edabrito7 <67392231+edabrito7@users.noreply.github.com>
2020-07-08 16:01:44 -0400

    Adding backend to github

---
+++ server.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+const bcrypt = require('bcrypt-nodejs');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '123',
+    database : 'smartbrain'
+  }
+});
+
+
+db.select('*').from('users').then(data =>{
+	console.log(data);
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+app.get('/', (req, res) =>{
+	res.send(database.users);
+})
+
+
+app.post('/signin', (req, res) => {
+	db.select('email', 'hash').from('login')
+	.where('email','=',req.body.email)
+	.then( data => {
+		const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+		if (isValid) {
+			return db.select('*').from('users')
+			.where('email','=',req.body.email)
+			.then( user => {
+				res.json(user[0])
+			})
+			.catch(err => res.status(400).json('unable to access'))
+		} else {
+			res.status(400).json("wrong credentials");
+		} 
+	})
+	.catch( err => res.status(400).json('wrong credentials'));
+})
+
+
+
+app.post('/register', (req, res) => {
+	const { email, name, lastname, password } = req.body;
+	const hash = bcrypt.hashSync(password);
+
+
+	db.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
+		})
+		.into('login')
+		.returning('email')
+		.then(loginEmail => {
+			return trx('users')
+			.returning('*')
+			.insert({
+				email: loginEmail[0],
+				name: name,
+				lastname: lastname,
+				joined: new Date()
+			}).then(user => {
+				res.json(user[0]);
+			})
+		})
+		.then(trx.commit)
+		.catch(trx.rollback)
+	})
+	.catch(err => res.status(400).json("Unable to register"));
+	
+	
+	
+})
+
+
+
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	db.select('*').from('users').where({id})
+	.then(user => {
+		if(user.length) {
+			res.json(user[0]);
+		} else {
+			res.json('Unable to get user');
+		}
+		
+	})
+	.catch(err => res.status(400).json("Unable to get user"));
+})
+
+
+app.put('/image', (req, res) => {
+	const { id } = req.body;
+	db('users').where('id','=',id)
+	.increment('entries',1)
+	.returning('entries')
+	.then( entries => {
+		res.json(entries[0])
+	}).catch( err => res.status(400).json("Unable to get entries"))
+		
+	})
+	
+
+
+
+
+app.listen(3001, () => {
+	console.log('app is running ok');
+})
+
+
+
+/*
+ / --> res = this is working  \/
+ /signin --> POST = success/fail \/
+ /register --> POST = user       \/
+ /profile/:userid --> GET = user
+ /image --> PUT --> user
+
+
+*/
